acd7114c09738a6104d33c1e2c06f6f59bcb7a5d
sadat.malik <sadat.malik@me.com>
2022-02-04 21:29:21 +0000

    Exercise 10

---
+++ src/main/java/com/sadatmalik/everythingisanobject/exercises/Exercise10.java
@@ -0,0 +1,10 @@
+package com.sadatmalik.everythingisanobject.exercises;
+
+// Write a program that prints three arguments taken from the command line.
+// To do this, you’ll need to index into the command-line array of Strings.
+public class Exercise10 {
+
+    public static void main(String[] args) {
+        System.out.println(args[0] + ", " + args[1] + ", " + args[2]);
+    }
+}
