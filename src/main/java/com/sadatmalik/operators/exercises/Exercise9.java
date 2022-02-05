180f6bcabb1f3b4c388b816349dc63cfa48d80ce
sadat.malik <sadat.malik@me.com>
2022-02-05 21:09:50 +0000

    operators exercise 9

---
+++ src/main/java/com/sadatmalik/operators/exercises/Exercise9.java
@@ -0,0 +1,20 @@
+package com.sadatmalik.operators.exercises;
+
+// Display the largest and smallest numbers for both float and double exponential notation.
+public class Exercise9 {
+
+    public static void main(String[] args) {
+        float fMax = Float.MAX_VALUE;
+        float fMin = Float.MIN_VALUE;
+
+        System.out.println(fMax);
+        System.out.println(fMin);
+
+        double dMax = Double.MAX_VALUE;
+        double dMin = Double.MIN_VALUE;
+
+        System.out.println(dMax);
+        System.out.println(dMin);
+    }
+
+}
