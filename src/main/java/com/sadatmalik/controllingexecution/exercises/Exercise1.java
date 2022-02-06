654a671bd982e0ee6dbaa2e60d9fecbb0835a60e
sadat.malik <sadat.malik@me.com>
2022-02-06 14:31:42 +0000

    controlling execution exercise 1

---
+++ src/main/java/com/sadatmalik/controllingexecution/exercises/Exercise1.java
@@ -0,0 +1,11 @@
+package com.sadatmalik.controllingexecution.exercises;
+
+// Write a program that prints values from 1 to 100.
+public class Exercise1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
