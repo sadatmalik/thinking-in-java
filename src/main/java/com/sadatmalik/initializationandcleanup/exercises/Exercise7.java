c9c1fb7cbf966eecabfd295ef7b496ebec293ea2
sadat.malik <sadat.malik@me.com>
2022-02-07 19:27:44 +0000

    initialisation and cleanup exercise 7

---
+++ src/main/java/com/sadatmalik/initializationandcleanup/exercises/Exercise7.java
@@ -0,0 +1,10 @@
+package com.sadatmalik.initializationandcleanup.exercises;
+
+// Create a class without a constructor, and then create an object of that class in
+// main( ) to verify that the default constructor is automatically synthesized.
+public class Exercise7 {
+
+    public static void main(String[] args) {
+        new Exercise7();
+    }
+}
