be719766d5873517a5cefaa289d5fcbbe5fa56ca
sadat.malik <sadat.malik@me.com>
2022-02-07 19:19:51 +0000

    initialisation and cleanup exercise 3

---
+++ src/main/java/com/sadatmalik/initializationandcleanup/exercises/Exercise3.java
@@ -0,0 +1,14 @@
+package com.sadatmalik.initializationandcleanup.exercises;
+
+// Create a class with a default constructor (one that takes no arguments)
+// that prints a message. Create an object of this class.
+public class Exercise3 {
+
+    public Exercise3() {
+        System.out.println("Exercise3() constructor called");
+    }
+
+    public static void main(String[] args) {
+        new Exercise3();
+    }
+}
