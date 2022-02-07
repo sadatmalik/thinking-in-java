be655fca3f4c03556988c0d8548d3e9d40411329
sadat.malik <sadat.malik@me.com>
2022-02-07 19:27:36 +0000

    initialisation and cleanup exercise 6

--- src/main/java/com/sadatmalik/initializationandcleanup/exercises/Exercise5.java
+++ src/main/java/com/sadatmalik/initializationandcleanup/exercises/Exercise5.java
@@ -4,10 +4,16 @@ package com.sadatmalik.initializationandcleanup.exercises;
 // overloaded based on various primitive data types, and print different types of barking,
 // howling, etc., depending on which overloaded version is called. Write a main( ) that
 // calls all the different versions.
+
+// Ex6. Modify the previous exercise so that two of the overloaded methods have two
+// arguments (of two different types), but in reversed order relative to each other. Verify
+// that this works.
 public class Exercise5 {
     public static void main(String[] args) {
         new Dog().bark(1);
         new Dog().bark(1f);
+        new Dog().bark(1, 1f);
+        new Dog().bark(1f, 1);
     }
 }
 
@@ -19,4 +25,13 @@ class Dog {
     public void bark(float f) {
         System.out.println("Floating like a piece of bark");
     }
+
+    public void bark(int i, float f) {
+        System.out.println("To integer, or to float, that is the question");
+    }
+
+    public void bark(float i, int f) {
+        System.out.println("To float, or to integer, that is the question");
+    }
+
 }
