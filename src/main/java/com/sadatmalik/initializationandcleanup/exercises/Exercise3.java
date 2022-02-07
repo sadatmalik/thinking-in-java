d23d5b09fc65268a7a369da3d12132b98f56e30f
sadat.malik <sadat.malik@me.com>
2022-02-07 19:21:39 +0000

    initialisation and cleanup exercise 4

--- src/main/java/com/sadatmalik/initializationandcleanup/exercises/Exercise3.java
+++ src/main/java/com/sadatmalik/initializationandcleanup/exercises/Exercise3.java
@@ -2,13 +2,21 @@ package com.sadatmalik.initializationandcleanup.exercises;
 
 // Create a class with a default constructor (one that takes no arguments)
 // that prints a message. Create an object of this class.
+
+// Ex4. Add an overloaded constructor to the previous exercise that takes a
+// String argument and prints it along with your message.
 public class Exercise3 {
 
     public Exercise3() {
         System.out.println("Exercise3() constructor called");
     }
 
+    public Exercise3(String s) {
+        System.out.println(s + " - " + "Exercise3() constructor called");
+    }
+
     public static void main(String[] args) {
         new Exercise3();
+        new Exercise3("Sadat");
     }
 }
